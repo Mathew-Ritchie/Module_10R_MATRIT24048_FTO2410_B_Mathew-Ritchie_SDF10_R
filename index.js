2e937e94cb44da74d6a9c69a059d6b217ffb8959
Mathew Ritchie <ritchmat89@gmail.com>
2024-11-22 14:38:17 +0200

    modifications to the JS so that we are connected to firebase database and have itens appearing on UI. also done css to style list items on UI.

--- index.js
+++ index.js
@@ -3,23 +3,46 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL:
     "https://playground-c67fe-default-rtdb.europe-west1.firebasedatabase.app",
 };
+
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-const shoppinglistInDB = ref(database, "shoppingList");
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
 
-  push(shoppinglistInDB, inputValue);
+  push(shoppingListInDB, inputValue);
 
-  console.log(inputValue);
+  clearInputFieldEl();
 });
+
+onValue(shoppingListInDB, function (snapshot) {
+  let itemsArray = Object.values(snapshot.val());
+  clearShoppingListEl();
+  for (let i = 0; i < itemsArray.length; i++) {
+    appendItemToShoppingListEl(itemsArray[i]);
+  }
+});
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function appendItemToShoppingListEl(itemValue) {
+  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
